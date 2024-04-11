7efc7981e74f3943534ed2665348cfc5e5e81e57
bobbibao <lehoangbao1125@gmail.com>
2024-04-11 16:56:34 +0700

    Big update for index.js with adding socket io
    - Integrate socket on setting up connection client and server
    
    - Created connection to each user through userId
    
    - Listening on join chat to send each room of users by roomId
    
    - Listening on (new message, delete message, unsend message, react message, typing, disconnect) to action in client and server

--- server/index.js
+++ server/index.js
@@ -1,32 +1,113 @@
 const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const userRoute = require("./routes/userRoute")
-const otpRoute = require("./routes/otpRoutes")
 const app = express();
+const bodyParser = require("body-parser");
+const router = require("./routes");
+const cors = require('cors');
+const { default: mongoose, now } = require("mongoose");
+const {createMeeting} = require('./api');
+
+const User = require('./models/user');
 
 require("dotenv").config();
-app.use(express.json());
-app.use(cors());
-app.use("/api/users", userRoute);
-app.use("/auth", otpRoute);
-//CRUD 
-
-app.get("/", (req, res) => {
-  res.send("Welcome our chat app APIs...");
-});
-
 const port = process.env.PORT || 3000;
-const uri = process.env.MONGO_URL;
+const mongodb_connect_string = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/test';
 
-app.listen(port, (req, res) => {
-  console.log(`Server running on port: ${port}`);
-});
+// Sử dụng cors middleware ở đầu ứng dụng để gg không chặn request
+app.use(cors());
 
-mongoose
-  .connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+// Chuyển đổi dữ liệu sang json và ngược lại
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(router);
+
+// Khởi chạy app
+mongoose.connect(mongodb_connect_string)
+  .then(() => {
+    const server = app.listen(port, () =>
+      console.log("> Server is up and running on port : http://localhost:" + port)
+    );
+    const io = require('socket.io')(server, {
+      pingTimeout: 60000,
+      cors: {
+        origin: '*',
+      }
+    });
+    io.on('connection', (socket) => {
+
+      socket.on('setup', async (userId) => {
+        try{
+          userId = JSON.parse(userId);
+          // socket.join(userId);
+          socket.userId = userId;
+          const user = await User.findById(userId);
+          user.isOnline = true;
+          await user.save();
+          socket.emit('setup', userId);
+        } catch(err) {
+          console.log(err);
+        }
+      });
+      socket.on("join chat", (room, userId) => {
+        userId = JSON.parse(userId);
+        socket.join(room);
+        socket.emit('join chat', room);
+        
+      });
+     
+      socket.on('message', (message, id) => {
+        const newMessage = {
+          id: id,
+          senderId: JSON.parse(message.senderId),
+          content: message.content,
+          time: now().getHours() + ':' + now().getMinutes(),
+          type: message.type,
+          media: message.media,
+        }
+        console.log('message', newMessage);
+        io.to(message.chatRoomId).emit('message', newMessage);
+      });
+
+      socket.on('delete message', (message) => {
+        console.log('delete message', message);
+        io.to(message.chatRoomId).emit('delete message', message);
+      });
+      
+      socket.on('unsend message', (message) => {
+        io.to(message.chatRoomId).emit('unsend message', {id: message.messageId});
+      });
+
+      socket.on('react message', (message) => {
+        io.to(message.chatRoomId).emit('react message', message);
+      });
+      // socket.on('typing', (data) => {
+      //   console.log('typing', data);
+      //   io.to(data.chatRoomId).emit('typing', data);
+      // });
+
+      // socket.on('stop typing', (data) => {
+      //   console.log('stop typing', data);
+      //   io.to(data.chatRoomId).emit('stop typing', data);
+      // });
+
+      socket.on('disconnect', async () => {
+        try{
+          console.log('user disconnected', socket.userId);
+          const user = await User.findById(socket.userId);
+          user.isOnline = false;
+          user.lastOnlineTime = Date.now();
+          await user.save();
+        } catch(err) {
+          console.log(err);
+        }
+      });
+
+      socket.on('call', (data) => {
+        createMeeting().then((meetingId) => {
+          io.to(data.chatRoomId).emit('call', meetingId);
+        });
+      });
+    });
   })
-  .then(() => console.log("MongoDB connection established"))
-  .catch((error) => console.log("MongoDB connection failed: ", error.message));
+  .catch(err =>
+    console.log(err));
