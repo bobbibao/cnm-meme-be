19a4e01cf9b4b957528f17787b0a5a5a9ef9b49d
bobbibao <lehoangbao1125@gmail.com>
2024-04-11 16:34:51 +0700

    Enhance for formating json api
    - Adjusted seachMessage route and recode getMessage from chatRoomId

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,61 +1,126 @@
 const Message = require("../models/message");
+const ChatRoom = require("../models/chatRoom");
+const Direct = require("../models/Direct");
 const ApiCode = require("../utils/apicode");
 const apiCode = new ApiCode();
+const { Types } = require('mongoose');
 
-//get list message
-const listMessage = (req, res) => {
-  Message.find()
-    .then((listMessage) => {
-        return res.json(apiCode.success(listMessage, "List All Message Success"));
-      })
-      .catch((err) => {
-        return res.json(apiCode.error(err, "List All Message Fail"));
-      });
-}
+require('dotenv').config();
+const AWS = require('aws-sdk');
+const { S3_BUCKET_NAME, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION } = process.env;
+const s3 = new AWS.S3({
+  accessKeyId: AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  region: AWS_REGION
+});
 
-// const getMessageById = (id) => {
-//   return Message.findById(id).exec();
-// }
+const getMessage = async (req, res) => {
+  const id = req.params.id;
 
-const sendMessage = (req, res) => {
-  const senderID = req.user.id;
-  const content = req.body.content;
-  const newMessage = new Message({ senderID, content });
-  newMessage.save()
-    .then((message) => {
-      return res.json(apiCode.success(message, "Send Message Success"));
-    })
-    .catch((err) => {
-      return res.json(apiCode.error(err, "Send Message Fail"));
-    });
-}
-
-const unsendMessage = async (req, res) => {
   try {
-    const { messageId } = req.params;
-    const message = await Message.findById(messageId);
-    
+    const message = await Message.findById(id);
     if (!message) {
-      return res.status(404).json({ error: 'Message not found' });
+      return res.status(404).json(apiCode.error("Message not found"));
     }
-
-    if (!req.user.id.equals(message.sender)) {
-      return res.status(403).json({ error: 'You can only unsend your own messages' });
+    else {
+      return res.status(200).json(apiCode.success(message, "Get Message Success"));
     }
-
-    message.content = null;
-    await message.save();
-
-    res.json({ message: 'Message has been unsent' });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Server error' });
+  } 
+  catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
 
+const getMessages = async (req, res) => {
+  try{
+    const chatRoomId = req.params.chatRoomId;
+    const direct = await Direct.findOne({ receiverId: { $ne: req.user.id }, chatRoomId: chatRoomId });
+    const chatRoom = await ChatRoom.findById(chatRoomId);
+    if(!chatRoom || !direct || !chatRoom.messages || chatRoom.messages.length === 0 || !chatRoom.messages[0]){
+      console.log('Messages not found');
+      return res.status(404).json(apiCode.error('Messages not found'));
+    }
+    else{
+      const messages = await Message.find({ _id: { $in: chatRoom.messages } });
+      const messageList = messages.map(message => {
+        return {
+          id: message._id,
+          content: message.content,
+          sent: req.user.id === message.senderID.toString(),
+          unsent: message.isDeleted,
+          isForwarded: message.isForwarded? true : false,
+          time: message.createAt.getHours() + ':' + message.createAt.getMinutes(),
+          reactions: message.reactions,
+          hided: message.hidedUsers.includes(new Types.ObjectId(req.user.id)),
+          type: message.type,
+          media: message.media
+        }
+      });
+      direct.unreadMessageCount = 0;
+      await direct.save();
+      return res.status(200).json(apiCode.success(messageList, 'Get Messages Success'));
+    }
+  }catch(error){
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const sendMessage = async (req, res) => {
+  const { chatRoomId, content } = req.body.data;
+  const ChatRoom = require('../models/chatRoom');
+  const direct = await Direct.findOne({ receiverId: { $eq: req.user.id }, chatRoomId: chatRoomId });
+  const newMessage = new Message({
+    senderID: req.user.id,
+    content: content,
+  });
+  const message = await newMessage.save();
+  const chatRoom = await ChatRoom.findById(chatRoomId);
+  chatRoom.messages.push(message._id);
+  chatRoom.lastMessage = message._id;
+  direct.unreadMessageCount += 1;
+  await direct.save();
+  await chatRoom.save();
+  return res.status(200).json(apiCode.success(message, 'Send Message Success'));
+};
+const searchMessages = async (req, res) => {
+  const chatRoomId = req.params.chatRoomId;
+  const keyword = req.query._q;
+  const chatRoom = await ChatRoom.findById(chatRoomId);
+  const messages = await Message.find({ _id: { $in: chatRoom.messages }, content: { $regex: keyword, $options: 'i' } });
+  const messageList = messages.map(message => {
+    return {
+      index: messages.indexOf(message),
+      id: message._id,
+      content: message.content,
+      sent: req.user.id === message.senderID.toString(),
+      time: message.createAt.getHours() + ':' + (message.createAt.getMinutes() < 10 ? '0' + message.createAt.getMinutes() : message.createAt.getMinutes())
+    }
+  });
+  return res.status(200).json(apiCode.success(messageList, 'Search Messages Success'));
+};
+const unsentMessage = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const message = await Message.findById(id);
+    if (!message) {
+      return res.status(404).json(apiCode.error("Message not found"));
+    }
+    else {
+      message.isDeleted = true;
+      await message.save();
+      return res.status(200).json(apiCode.success(message, "Unsent Message Success"));
+    }
+  }
+  catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 module.exports = {
-  listMessage,
+  getMessage,
+  getMessages,
+  searchMessages,
   sendMessage,
-  unsendMessage
+  unsentMessage,
+
 }
--- server/routes/site.js
+++ server/routes/site.js
@@ -1,43 +1,44 @@
 const router = require("express").Router();
 const authenticateJWT = require('../middleware/auth');
+const express = require('express');
+const cookieParser = require('cookie-parser');
 
-// some functions for testing, not used in the final version
-const {
-    listMessage,
-    sendMessage,
-    unsendMessage } = require("../controllers/messageController");
+const app = express();
 
-const {registerUser, loginUser, resetPassword, forgotPassword, updateUser, getProfile, updateAvatar,
-        searchUser, addFriend, acceptFriend, getAllFriendRequest, getUserProfile, getUserByChatRoomId, getUser} 
-= require('../controllers/userController');
+app.use(express.json());
+app.use(cookieParser());
 
-const { getGroupDetail, getGroupDetails } = require('../controllers/groupController');
+const otpController = require("../controllers/otpController");
+const { getGroupDetail, getGroupDetails } = require('../controllers/groupDetailController');
 const { getDirect, getDirects, getInfoChatItem } = require('../controllers/directController');
 const { getChatRoom, getChatRoomByDirectId } = require('../controllers/chatRoomController');
 
-const otpController = require("../controllers/otpController");
+const {registerUser, loginUser, resetPassword, forgotPassword, updateUser, getProfile, updateAvatar,
+    searchUser, addFriend, acceptFriend, getAllFriendRequest, getUserProfile, getUserByChatRoomId, getUser} 
+        = require('../controllers/userController');
 
-const { 
-    listDirect, 
-    getMessages } = require("../controllers/directController");
 
+const { getMessage, getMessages, searchMessages, unsentMessage, sendMessage } = require('../controllers/messageController');
+
+//Group detail
 router.get('/groupDetail/:id', authenticateJWT, getGroupDetail);
 router.get('/groupDetails/', authenticateJWT, getGroupDetails);
 
+//Direct
 router.get('/direct/:id', authenticateJWT, getDirect);
 router.get('/directs/', authenticateJWT, getDirects);
 router.get('/info-chat-item/', authenticateJWT, getInfoChatItem);
 
+//Chat room
 router.get('/chatRoom/:id', authenticateJWT, getChatRoom);
 router.get('/chat-room/:directId', authenticateJWT, getChatRoomByDirectId);
 
-router.get("/list-message", listMessage);
-router.post("/direct/:direct/send-message", authenticateJWT, sendMessage);
-router.get("/direct/:direct/get-messages", authenticateJWT, getMessages);
-router.patch("/direct/:direct/unsend-message/:messageId, ", authenticateJWT, unsendMessage);
-// router.get("/direct/:direct/search-messages, ", authenticateJWT, searchMessages);
-
-router.get("/list-direct", authenticateJWT, listDirect);
+//Message
+router.get('/message/:id', authenticateJWT, getMessage);
+router.get('/messages/:chatRoomId', authenticateJWT, getMessages);
+router.post('/search-messages', authenticateJWT, searchMessages);
+router.post('/send-message/', authenticateJWT, sendMessage);
+router.patch('/unsent-message/:id', authenticateJWT, unsentMessage);
 
 //user management and authentication
 router.get('/user/:id', authenticateJWT, getUser);
@@ -48,6 +49,7 @@ router.post("/users/forgot-password", forgotPassword);
 router.post("/users/reset-password/:id/:token", resetPassword);
 router.post("/users/verify", otpController.verifyOTP);
 
+// profile management
 router.get("/profile", authenticateJWT, getProfile);
 router.post("/profile", authenticateJWT, updateUser);
 router.get("/profile/:username", authenticateJWT, getUserProfile);
@@ -56,8 +58,6 @@ router.post('/add-friend', authenticateJWT, addFriend);
 router.post('/accept-friend', authenticateJWT, acceptFriend);
 router.get("/getAllFriendRequest", authenticateJWT, getAllFriendRequest);
 router.post('/search-user', authenticateJWT, searchUser);
-
-
 
 router.get('/info-user/:chatRoomId', authenticateJWT, getUserByChatRoomId);
 
